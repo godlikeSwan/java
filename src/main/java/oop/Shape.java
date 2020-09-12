cf7e383580eb93d6bf6bc6efc6f17438734fc7fc
Lebedev <swan@pc.localdomain>
2020-09-12 11:09:18 +0000

    added Shape class

---
+++ src/main/java/oop/Shape.java
@@ -0,0 +1,28 @@
+package oop;
+
+public class Shape {
+    Stirng name;
+    int numberOfLines;
+    int P;
+    int S;
+    String color;
+    public Shape () {
+        name = "dot";
+        numberOfLines = 0;
+        P = 0;
+        S = 0;
+        color = "black";
+    }
+    public Shape (String name, String color, int S, int P) {
+        this.name = name;
+        this.color = color;
+        this.S = S;
+        this.P = P;
+        if (name == "Square") {
+            this.numberOfLines = 4;
+        }
+    }
+    public String toString () {
+        return name + ", " + numberOfLines + " lines, " + "color: " + color + " P: " + P + " S: " + S;
+    }
+}
